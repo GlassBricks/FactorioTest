d0ae1d90e269f9534e1646d47645bc52a1806c73
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-11-28 19:27:20 -0600

    Hide factorio-test self-test unless actually auto-started

--- mod/control.ts
+++ mod/control.ts
@@ -2,6 +2,9 @@ import "./control/index"
 import { Settings } from "./constants"
 
 // Only load self-tests with auto start, so users get less confused
-if (settings.startup[Settings.AutoStart]!.value === true) {
+if (
+  settings.startup[Settings.AutoStart]!.value === true &&
+  settings.global[Settings.ModToTest]!.value === script.mod_name
+) {
   require("__factorio-test__/init")(["test.meta.test", "test.reload.test"])
 }
